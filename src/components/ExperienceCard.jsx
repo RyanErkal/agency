4567267f9e37085cd2624bac2621af882666f9f0
Ryan Erkal <ryanerkal@yahoo.ie>
2024-01-04 16:19:38 +0000

    mockups

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -5,15 +5,30 @@ import layout3 from "../../public/layout3.png";
 
 export default function ExperienceCard({ title, services, right }) {
     return (
-        <a href="/project/1" className={`bg-slate-800 text-slate-900 border-2 border-purple-200 flex flex-row items-${right ? "right" : "left"} justify-between rounded-lg`}>
-            <div className={`flex flex-col items-${right ? "end" : "start"} justify-start absolute p-8 w-full`}>
-                <h1 className={`font-bold bg-purple-200 px-4 py-2 mb-2 text-xl w-fit rounded-full ${right ? "text-right" : null}`}>{title}</h1>
-                {services.map((service) => {
-                    return <h2 key={service} className={`font-bold bg-purple-200 px-4 py-2 mb-2 text-sm w-fit rounded-full ${right ? "text-right" : null}`}>{service}</h2>;
-                }
-                )}
-            </div>
-            <img src={right ? layout1 : layout3} className="object-contain " />
-        </a>
+        right ?
+            (<a href="/project/1" className={`bg-slate-800 text-slate-900 border-2 border-purple-200 flex flex-row items-start justify-between rounded-lg`}>
+                <div className={`flex flex-col items-end justify-start absolute p-8 w-full`}>
+                    <h1 className={`font-bold bg-purple-200 px-4 py-2 mb-2 text-md xl:text-xl w-fit rounded-full `}>{title}</h1>
+                    {services.map((service) => {
+                        return <h2 key={service} className={`font-bold bg-purple-200 px-4 py-2 mb-2 text-xs xl:text-sm w-fit rounded-full `}>{service}</h2>;
+                    }
+                    )}
+                </div>
+                <img src={right ? layout1 : layout3} className="object-contain " />
+            </a>
+            ) : (
+                <a href="/project/1" className="bg-slate-800 text-slate-900 border-2 border-purple-200 flex flex-row items-start justify-between rounded-lg">
+                    <div className="flex flex-col items-start justify-start absolute p-8 w-full">
+                        <h1 className="font-bold bg-purple-200 px-4 py-2 mb-2 text-md xl:text-xl w-fit rounded-full text-right">{title}</h1>
+                        {services.map((service) => {
+                            return <h2 key={service} className="font-bold bg-purple-200 px-4 py-2 mb-2 text-xs xl:text-sm w-fit rounded-full text-right">{service}</h2>;
+                        }
+                        )}
+                    </div>
+                    <img src={right ? layout1 : layout3} className="object-contain " />
+                </a>
+            )
     );
 }
+
+
